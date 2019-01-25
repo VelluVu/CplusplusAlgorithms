162e2c98886ac9e4261bfc052fcbed96f816f7d1
VelluVu <velimavu@metropolia.fi>
2019-01-25 07:02:00 +0200

    taulu harjoitus 4

--- Harjoitukset/Harjoitus4/array/array.h
+++ Harjoitukset/Harjoitus4/array/array.h
@@ -25,12 +25,15 @@ template<class T>
 inline Array<T>::Array()
 {
 	// Varaa oletuskokoiselle (5) taulukolle tilaa heapista (new).
+	_array = new T*[5];
+	_size = 5;
 }
 
 template<class T>
 inline Array<T>::~Array()
 {
 	// Vapauta taulukon tilanvaraus heapista (delete).
+	delete[] _array;
 }
 
 template<class T>
@@ -38,6 +41,7 @@ inline T & Array<T>::operator[](unsigned i)
 {
 	// Palauta taulukon alkio indeksiss� i.
 	// Virhetarkistusta ei tarvita.
+	return _array[i];
 }
 
 template<class T>
@@ -45,13 +49,26 @@ inline bool Array<T>::find(const T & item) const
 {
 	// Tutki, l�ytyyk� item taulukosta. Palauta true/false.
 	// Kirjoita algoritmi itse.
-
+	for (size_t i = 0; i < _size; i++)
+	{
+		if (_array[i] == item) {
+			return true;
+		}
+	}
 	return false;
 }
 
 template<class T>
 inline void Array<T>::resize(unsigned s)
 {
+
+	T* new_array = new T[s];
+	for (unsigned i = 0; i < _size && i < s; ++i) {
+		new_array[i] = _array[i];
+	}
+	delete[] _array;
+	_array = new_array;
+	_size = s;
 	// Kasvata tai pienenn� taulukko uuteen kokoon s.
 	// Vanhat arvot tulee kopioida uuteen taulukkoon
 	// (niin monta kuin mahtuu, jos taulukko pieneni).
@@ -62,7 +79,7 @@ template<class T>
 inline unsigned Array<T>::size() const
 {
 	// Palauttaa taulukon koon.
-	return 0;
+	return _size;
 }
 
 template<class T>
@@ -73,4 +90,13 @@ inline void Array<T>::squeeze_in(unsigned i, const T & item)
 	// T�m�n pit�� toimia oikein my�s silloin, jos lis�ys
 	// halutaan tehd� taulukon ensimm�iseksi tai viimeiseksi
 	// alkioksi (tai jos taulukon koko on 0).
+	unsigned koko = _size + 1;
+	unsigned temp = _size;
+	resize(koko);
+	
+	while (temp >= i) {
+		_array[temp + 1] = _array[temp];
+		temp -= 1;
+	}
+	_array[i] = item;
 }
