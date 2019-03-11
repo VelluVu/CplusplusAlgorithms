72ca5232d95f6a762f288bd4dd3358cbdd8b4e29
VelluVu <velimavu@metropolia.fi>
2019-03-11 13:55:04 +0200

    pieni korjaus rekursio

--- RekursioHarjoitukset/RekursioHarjoitukset/RekursioHarjoitukset/Header.h
+++ RekursioHarjoitukset/RekursioHarjoitukset/RekursioHarjoitukset/Header.h
@@ -10,9 +10,9 @@ b) Toteuta vastaava funktio rekursiivisesti.
 void IteratiivinenFunktio(int * lukuJono, int size, int montakoKertaa) 
 {
 
-	for (int i = 0; i < montakoKertaa; i++)
+	for (int i = 0; i <= montakoKertaa; i++)
 	{
-		cout << lukuJono[0] << ", ";
+		cout << lukuJono[i] << ", ";
 	}
 	cout << endl;
 
@@ -20,15 +20,12 @@ void IteratiivinenFunktio(int * lukuJono, int size, int montakoKertaa)
 
 void RekursiivinenFunktio(int * lukuJono, int montakoKertaa) 
 {
-
-	if (montakoKertaa != 0)
+	
+	if (montakoKertaa >= 0)
 	{
-		cout << lukuJono[0] << ", ";
 		RekursiivinenFunktio(lukuJono, montakoKertaa - 1);
+		cout << lukuJono[montakoKertaa] << ", ";
 	}
-	else 
-	{
-		return;
-	}
+	
 
 }
